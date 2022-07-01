911cab1d1058166a623fad5eff0ba14935e73374
GaiUyama <107541106+GaiUyama@users.noreply.github.com>
2022-07-01 15:13:48 +0900

    Update csa_to_hcpe.py

--- utils/csa_to_hcpe.py
+++ utils/csa_to_hcpe.py
@@ -16,6 +16,7 @@ parser.add_argument('--test_ratio', type=float, default=0.1)
 args = parser.parse_args()
 
 # glob.glob(args.csa_dir/**/*.csa) 
+# ファイルパスをリストとして取得
 # **: 同一階層以外のファイルを探す, recursive=True: ファイルを再帰的に探す
 csa_file_list = glob.glob(os.path.join(args.csa_dir, '**', '*.csa'), recursive=True)
 
@@ -24,10 +25,13 @@ file_list_train, file_list_test = train_test_split(csa_file_list, test_size=args
 
 hcpes = np.zeros(1024, HuffmanCodedPosAndEval)
 
+# hcpe_train = train.hcpe, hcpe_test = test.hcpe
+# 'wb' = バイナリファイルを書き込みモードで開く
 f_train = open(args.hcpe_train, 'wb')
 f_test = open(args.hcpe_test, 'wb')
 
 board = Board()
+# zip() : file_list = [file_list_train, file_list_test], f = [f_train, f_test]
 for file_list, f in zip([file_list_train, file_list_test], [f_train, f_test]):
     kif_num = 0
     position_num = 0
@@ -35,6 +39,7 @@ for file_list, f in zip([file_list_train, file_list_test], [f_train, f_test]):
         for kif in CSA.Parser.parse_file(filepath):
             # 投了、千日手、宣言勝ちで終了した棋譜以外を除外
             if kif.endgame not in ('%TORYO', '%SENNICHITE', '%KACHI'):
+                # 処理をスキップ
                 continue
             # 手数が少ない棋譜を除外
             if len(kif.moves) < args.filter_moves:
@@ -46,7 +51,9 @@ for file_list, f in zip([file_list_train, file_list_test], [f_train, f_test]):
             # 開始局面を設定
             board.set_sfen(kif.sfen)
             p = 0
+            # 例外が発生するかもしれないが、実行したい処理
             try:
+                # enumerate() : 要素のインデックスと要素を同時に取り出す
                 for i, (move, score, comment) in enumerate(zip(kif.moves, kif.scores, kif.comments)):
                     # 不正な指し手のある棋譜を除外
                     if not board.is_legal(move):
@@ -64,8 +71,10 @@ for file_list, f in zip([file_list_train, file_list_test], [f_train, f_test]):
                     # 勝敗結果
                     hcpe['gameResult'] = kif.win
                     board.push(move)
+            # 例外発生：除外対象の棋譜であるとき
             except:
                 print(f'skip {filepath}')
+                # 処理をスキップ
                 continue
 
             if p == 0:
