ad9d7aafa436c6434bf3c61fc6deed27cf7ca074
GaiUyama <107541106+GaiUyama@users.noreply.github.com>
2022-07-19 04:59:57 +0900

    Update make_priority.py

--- pydlshogi2/make_priority.py
+++ pydlshogi2/make_priority.py
@@ -1,15 +1,15 @@
+import math
 from cshogi import *
 
-# 対局結果から報酬を作成
-def make_result(game_result, color):
+def make_priority(eval, game_result, color):
     if color == BLACK:
         if game_result == BLACK_WIN:
-            return 1000
+            return math.fabs(1000-eval)
         if game_result == WHITE_WIN:
-            return -1000
+            return math.fabs(-1000-eval)
     else:
         if game_result == BLACK_WIN:
-            return -1000
+            return math.fabs(-1000-eval)
         if game_result == WHITE_WIN:
-            return 1000
-    return 0
+            return math.fabs(1000-eval)
+    return math.fabs(eval)
