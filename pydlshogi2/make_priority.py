eb5c6f372c845f5279fc25e2872833bd58c56622
GaiUyama <107541106+GaiUyama@users.noreply.github.com>
2022-08-07 22:36:35 +0900

    Update make_priority.py

--- pydlshogi2/make_priority.py
+++ pydlshogi2/make_priority.py
@@ -1,8 +1,12 @@
 import math
+import numpy as np
 
 from cshogi import *
 
-def make_priority(eval, game_result, color):
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def e_make_priority(eval, game_result, color):
     if color == BLACK:
         if game_result == BLACK_WIN:
             return math.fabs(1000-eval)
@@ -14,3 +18,5 @@ def make_priority(eval, game_result, color):
         if game_result == WHITE_WIN:
             return math.fabs(1000-eval)
     return math.fabs(eval)
+
+def r_make_priority():
