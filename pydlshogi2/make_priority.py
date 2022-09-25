ba80ef24d89572e299a1737c59329d9c160384b2
GaiUyama <107541106+GaiUyama@users.noreply.github.com>
2022-09-26 02:54:26 +0900

    Update make_priority.py

--- pydlshogi2/make_priority.py
+++ pydlshogi2/make_priority.py
@@ -8,10 +8,11 @@ def sigmoid(x):
 
 def make_priority(eval, game_result, color):
     if color == BLACK:
-        if game_result == BLACK_WIN:
-            return math.fabs(100000-eval)
-        else:
+        if game_result == BLACK_WIN:  #先手勝ち
+            #return math.fabs(100000-eval)
             return 0
+        else:  #先手負け
+            return 1
         '''
         if game_result == WHITE_WIN:
             return math.fabs(-100000-eval)
@@ -24,10 +25,11 @@ def make_priority(eval, game_result, color):
         else:
             return 0
         '''
-        if game_result == WHITE_WIN:
-            return math.fabs(100000-eval)
-        else:
+        if game_result == WHITE_WIN:  #後手勝ち
+            #return math.fabs(100000-eval)
             return 0
+        else:  #後手負け
+            return 1
             
     return 0 # math.fabs(eval)
 
